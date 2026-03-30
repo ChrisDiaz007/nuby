8849a62e32d1f14087947d5b736204f37d4d4c2d
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2026-03-30 17:30:07 -0400

    Add progress bar and weekly calorie chart to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,21 @@
 class DashboardController < ApplicationController
-
   def show
-    @today_logs = current_user.food_logs.includes(:food).where(logged_on: Date.today).order(:meal_type)
+    @today_logs = current_user.food_logs
+                              .includes(:food)
+                              .where(logged_on: Date.today)
+                              .order(:meal_type)
 
     @meals = @today_logs.group_by(&:meal_type)
     @total_calories = @today_logs.sum { |log| (log.food.calories_per_serving || 0) * log.servings }
+
+    @calorie_profile = current_user.calorie_profile
+    @daily_target = @calorie_profile&.daily_target || 2000
+
+    @weekly_data = (6.downto(0)).map do |days_ago|
+      date = Date.today - days_ago
+      logs = current_user.food_logs.includes(:food).where(logged_on: date)
+      total = logs.sum { |log| (log.food.calories_per_serving || 0) * log.servings }
+      { date: date.strftime("%a"), calories: total.round }
+    end
   end
 end
