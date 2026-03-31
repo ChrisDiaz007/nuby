7b390354daf0280849ce874ae0ad02778c14bc51
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2026-03-31 14:42:27 -0400

    Added CSS and reworked pages

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,10 @@
 class DashboardController < ApplicationController
   def show
+    @selected_date = params[:date] ? Date.parse(params[:date]) : Date.today
+
     @today_logs = current_user.food_logs
                               .includes(:food)
-                              .where(logged_on: Date.today)
+                              .where(logged_on: @selected_date)
                               .order(:meal_type)
 
     @meals = @today_logs.group_by(&:meal_type)
@@ -15,7 +17,7 @@ class DashboardController < ApplicationController
       date = Date.today - days_ago
       logs = current_user.food_logs.includes(:food).where(logged_on: date)
       total = logs.sum { |log| (log.food.calories_per_serving || 0) * log.servings }
-      { date: date.strftime("%a"), calories: total.round }
+      { date: date.strftime("%a"), full_date: date.to_s, calories: total.round }
     end
   end
 end
