a357c1b0aee06f2dee238b49ba67beaf9dd634b1
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2026-03-30 21:00:39 -0400

    Add color-coded nutri-score badge to food cards and saved food list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def nutri_score_color(score)
+    case score&.downcase
+    when "a" then "bg-green-500 text-white"
+    when "b" then "bg-lime-400 text-white"
+    when "c" then "bg-yellow-400 text-black"
+    when "d" then "bg-orange-400 text-white"
+    when "e" then "bg-red-500 text-white"
+    else "bg-gray-300 text-gray-600"
+    end
+  end
 end
